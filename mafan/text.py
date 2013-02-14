21f1c154088813da74702d4874489c1edfd94fb1
Herman Schaaf <hermanschaaf@gmail.com>
2013-02-14 21:59:33 +0800

    added pinyin and chinese checking functions

---
+++ mafan/text.py
@@ -0,0 +1,29 @@
+#encoding=utf-8
+"""
+Some helpful functions for parsing or changing Chinese text
+"""
+import re
+from HTMLParser import HTMLParser
+
+english = re.compile('[\w\~\!\s\@\#\$\%\^\&\*\(\)]+')
+known_punctuation = u'／（）、，。：「」…。'
+
+def contains_english(unicode_string):
+  """Attempts to determine whether the string contains any of the common English characters."""
+  
+  if english.search(unicode_string):
+    return True
+  return False
+
+
+def contains_latin(unicode_string):
+  """Attempts to determine whether the string contains any non-Asian scripts"""
+  string = unicode_string.encode("ascii", "ignore")
+  return contains_english(string)
+
+
+def is_punctuation(word):
+  """
+  Check if a string is among any of the common Chinese punctuation.
+  """
+  return word in known_punctuation
