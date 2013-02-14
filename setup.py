f5bec14fc9219225c1ca243484c0aa1534c53355
Herman Schaaf <hermanschaaf@gmail.com>
2013-02-14 22:56:57 +0800

    fixed manifest.in to include markdown

--- setup.py
+++ setup.py
@@ -2,15 +2,15 @@ from distutils.core import setup
 
 setup(
     name='mafan',
-    version='0.1.0',
+    version='0.1.1',
     author='Herman Schaaf',
     author_email='herman@ironzebra.com',
     packages=['mafan'],
     scripts=['bin/convert.py'],
-    url='http://pypi.python.org/pypi/mafan/',
+    url='https://github.com/hermanschaaf/mafan',
     license='LICENSE.txt',
     description='A toolbox for working with the Chinese language in Python',
-    long_description=open('README.md').read(),
+    long_description=open('docs/README.md').read(),
     install_requires=[
         "argparse == 1.2.1",
         "chardet == 2.1.1",
