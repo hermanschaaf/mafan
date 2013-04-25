b40abf8996ade1d2090eed46745f2d49f17a223b
Herman Schaaf <hermanschaaf@gmail.com>
2013-04-25 22:33:01 +0900

    Updated tests

--- mafan/tests/text_test.py
+++ mafan/tests/text_test.py
@@ -10,33 +10,23 @@ The way to run this right now is to make your current directory the parent of th
 This places the mafan package on the relative path of this file.
 """
 
+import unittest
 
 from ..text import *
-
 
 st = u"聲音鳥樹葉話説話細又輕蝴蝶請只有和得聼得到蜜蜂"
 st2 = u'这是麻烦啦'
 
+class TestIdentifyFunction(unittest.TestCase):
 
-print "Testing %s ..." % "test"
-print "is_simplified: %s" % is_simplified("test")
+    def test_simplify(self):
+        self.assertEqual(simplify(st2), st2)
 
-print "Testing %s ..." % st
-print "is_simplified: %s" % is_simplified(st)
-assert is_simplified(st) == False, "Failed test: is_simplified"
-print "is_traditional: %s" % is_traditional(st)
-assert is_traditional(st) == True, "Failed test: is_traditional"
+    def test_tradify(self):
+        self.assertEqual(tradify(st2), u'這是麻煩啦')
 
-print "Testing %s ..." % st2
-print "is_simplified: %s" % is_simplified(st2)
-assert is_simplified(st2) == True, "Failed test: is_simplified2"
-print "is_traditional: %s" % is_traditional(st2)
-assert is_traditional(st2) == False, "Failed test: is_traditional2"
+    def test_simplify_and_tradify(self):
+        self.assertEqual(simplify(tradify(st2)), st2)
 
-print "Passed simplified and traditional tests."
-
-assert tradify(st2) == u'這是麻煩啦', "Failed test: tradify"
-assert simplify(st2) == st2, "Failed test: simplify"
-assert simplify(tradify(st2)) == st2, "Failed test: text -> tradify -> simplify"
-
-print "Passed all tests!"
+if __name__ == '__main__':
+    unittest.main()
