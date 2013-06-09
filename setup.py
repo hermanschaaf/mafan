69c89d1e63dd4a965c293ed1c626b0d72cd2a424
Herman Schaaf <hermanschaaf@gmail.com>
2013-06-09 17:48:47 +0900

    Update text punctuation

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='mafan',
-    version='0.2.1',
+    version='0.2.2',
     author='Herman Schaaf',
     author_email='herman@ironzebra.com',
     packages=['mafan', 'mafan.hanzidentifier'],
